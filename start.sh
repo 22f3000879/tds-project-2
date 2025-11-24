0b52dd61f2f988de49709f8bb768ad913d3aac39
Aryan Jha <133153671+22f3000879@users.noreply.github.com>
2025-11-24 23:26:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo "Installing Playwright Chromium..."
-python -m playwright install --with-deps chromium
-
 echo "Starting FastAPI server..."
 uvicorn app.main:app --host 0.0.0.0 --port $PORT
