7ad3ea207cb360434e190f99d145a2a9a59b3943
Aryan Jha <133153671+22f3000879@users.noreply.github.com>
2025-11-24 23:02:15 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,3 +1,3 @@
-SECRET = "22f3000879"
-EMAIL  = "22f3000879@ds.study.iitm.ac.in"
-TIMEOUT_SECONDS = 165   # stay under 3 minutes (180s)
+EMAIL = "22f3000879@ds.study.iitm.ac.in"
+SECRET = "22f3000879" 
+AIPIPE_VAR = "AI_PIPE"  # environment variable name
