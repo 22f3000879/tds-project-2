edf1566686cecade13e8d4009661fc83fc6dde42
Aryan Jha <133153671+22f3000879@users.noreply.github.com>
2025-11-25 00:30:06 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ EMAIL = "22f3000879@ds.study.iitm.ac.in"
 SECRET = "22f3000879"
 
 AIPIPE_TOKEN = os.getenv("AIPIPE_TOKEN")
-AIPIPE_API = "https://api.aipipe.io/v1/browser"
-AIPIPE_LLM = "https://api.aipipe.io/v1/llm"
+AIPIPE_API = "https://api.aipipe.org/v1/browser"
+AIPIPE_LLM = "https://api.aipipe.org/v1/llm"
