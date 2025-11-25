bd4bc628c4682b96d28bf331b55d2ee2e24b4fc0
Aryan Jha <133153671+22f3000879@users.noreply.github.com>
2025-11-25 21:12:26 +0530

    Create config.py

---
+++ app/config.py
@@ -0,0 +1,10 @@
+import os
+
+EMAIL = "22f3000879@ds.study.iitm.ac.in"
+SECRET = "22f3000879"
+
+# AiPipe LLM Token
+AIPIPE_TOKEN = os.getenv("AIPIPE_TOKEN")
+
+# AiPipe OpenAI-compatible endpoint
+AIPIPE_BASE = "https://aipipe.org/openai/v1/chat/completions"
